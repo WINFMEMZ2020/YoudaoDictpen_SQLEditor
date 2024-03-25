fd37d268da9ca147c4717f6ecb31e5005800409c
evnrowa <123802298+evnrowa@users.noreply.github.com>
2024-03-25 23:01:26 +0800

    feat：增加启动ssh，利用sftp远程操作功能

--- item_add.py
+++ item_add.py
@@ -6,6 +6,142 @@ import time
 import json
 import subprocess
 import colorama
+import paramiko
+
+
+
+
+class SFTP:
+    def __init__(self):
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.connection_start_time = None
+
+    def connect(self, ip, username, password):
+        self.connection_start_time = time.time()
+        try:
+            self.ssh_client.connect(ip, username=username, password=password, timeout=10)
+        except paramiko.AuthenticationException:
+            print("Authentication failed. Please check your credentials.")
+        except paramiko.SSHException as e:
+            end_time = time.time()
+            wait_time = int(end_time - self.connection_start_time)
+            print(f"Connection failed after {wait_time} seconds: {e}")
+        except Exception as e:
+            end_time = time.time()
+            wait_time = int(end_time - self.connection_start_time)
+            print(f"Error: {e}")
+
+    def bytes_to_mib(self, bytes_value):
+        mib_value = bytes_value / (1024 * 1024)
+        return mib_value
+
+    def _execute_sftp(self, local_files, remote_path):
+        # 连接SSH服务器
+        if not self.ssh_client.get_transport() or not self.ssh_client.get_transport().is_active():
+            print("SSH connection is not active.")
+            print("连接失败，请检查连接，按enter退出")
+            os.system("pause")
+            os._exit(1)
+
+        # 创建 SFTP 客户端
+        sftp_client = self.ssh_client.open_sftp()
+
+        # 上传文件到远程服务器
+        for file_info in local_files:
+            local_file_path = file_info["path"]
+            remote_file_path = os.path.join(remote_path, file_info["name"])
+            total_bytes = os.path.getsize(local_file_path)
+
+            # 检查远程文件是否存在，并获取已传输的大小
+            try:
+                remote_file_size = sftp_client.stat(remote_file_path).st_size
+            except FileNotFoundError:
+                remote_file_size = 0
+
+            # 定义进度回调函数
+            def progress_callback(bytes_transferred, file_size):
+                bytes_transferred_mib = self.bytes_to_mib(bytes_transferred)
+                file_size_mib = self.bytes_to_mib(file_size)
+                progress = int(bytes_transferred_mib / file_size_mib * 100)
+                print(f"\rProgress: {progress}% ({bytes_transferred_mib:.2f} MiB / {file_size_mib:.2f} MiB) - Uploading: {file_info['name']} - Elapsed Time: {self.get_elapsed_time()}", end='', flush=True)
+
+                # 判断是否上传完成
+                if bytes_transferred == file_size:
+                    print(f"\nUpload complete: {file_info['name']}")
+
+            # 上传文件
+            retries = 3  # 设置重试次数
+            for attempt in range(retries):
+                try:
+                    with open(local_file_path, 'rb') as local_file:
+                        # 移动文件指针至已传输的位置
+                        local_file.seek(remote_file_size)
+                        # 从已传输位置开始上传文件，并传入进度回调函数
+                        sftp_client.putfo(local_file, remote_file_path, callback=lambda x, y: progress_callback(x + remote_file_size, total_bytes), file_size=total_bytes)
+                    break  # 上传成功，跳出重试循环
+                except paramiko.SSHException as e:
+                    print(f"SSHException occurred during upload: {e}")
+                    if attempt < retries - 1:
+                        print("Retrying upload...")
+                        time.sleep(5)  # 等待一段时间后重试
+                    else:
+                        print("Upload failed after retries.")
+                        raise  # 上传失败，抛出异常
+
+        # 关闭连接
+        sftp_client.close()
+
+        print("\nAll files uploaded.")
+
+        # 返回上传完成信息
+        return "All files uploaded."
+
+    
+    def execute_sftp(self, local_files, remote_path):
+        if not self.ssh_client.get_transport() or not self.ssh_client.get_transport().is_active():
+            print("SSH connection is not active.")
+            print("连接失败，请检查连接，按enter退出")
+            os.system("pause")
+            os._exit(1)
+        sftp_client = self.ssh_client.open_sftp()
+        local_file_path = local_files
+        remote_file_path = remote_path
+        total_bytes = os.path.getsize(local_file_path)
+        with open(local_file_path, 'rb') as local_file:
+            sftp_client.putfo(local_file, remote_file_path)
+        sftp_client.close()
+        print("\nAll files uploaded.")
+        return "All files uploaded."
+
+
+    def get_elapsed_time(self):
+        if self.connection_start_time is not None:
+            elapsed_time = int(time.time() - self.connection_start_time)
+            return f"{elapsed_time} seconds"
+        else:
+            return "N/A"
+
+    def pull_file(self, remote_path, local_path):
+        if not self.ssh_client.get_transport() or not self.ssh_client.get_transport().is_active():
+            print("SSH connection is not active.")
+            print("连接失败，请检查连接，按enter退出")
+            os.system("pause") 
+            os._exit(1)
+        # 创建 SFTP 客户端
+        sftp_client = self.ssh_client.open_sftp()
+
+        # 下载文件
+        try:
+            sftp_client.get(remote_path, local_path)
+            print(f"File downloaded successfully to {local_path}")
+        except Exception as e:
+            print(f"Error downloading file: {e}")
+
+        # 关闭连接
+        sftp_client.close()
+
+sftp = SFTP()
 
 ####
 # 读取config.json文件
@@ -14,17 +150,19 @@ with open('config.json', 'r') as f:
 
 dictpen_password = config.get("dictpen_password","")
 copy_video_to_dictpen_state = config.get("copy_video_to_dictpen","")
+ip_address = config.get("ip_address","")
+remote_copy_status = config.get("remote_copy_video_to_dictpen","")
+dictpen_root = config.get("dictpen_root","")
 #####
 
 #这是用以获取所有后缀是.mp4的文件的函数
 def get_mp4_files(directory):
-    mp4_files = []  
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".mp4"):
-                file_path = os.path.join(root, file)  # 视频文件的绝对路径
-                file_name = os.path.basename(file_path)
-                mp4_files.append({"path": file_path, "name": file_name})
+    mp4_files = [
+        {"path": os.path.join(root, file), "name": file}
+        for root, dirs, files in os.walk(directory)
+        for file in files
+        if file.endswith(".mp4")
+    ]
     return mp4_files
 
 
@@ -118,15 +256,23 @@ else:
     print("已禁用：同时将视频文件从电脑拷至词典笔")
 os.system("pause")
 
-#检查设备是否处于OK状态
-check_devices_okay()
 current_time = time.localtime()
 #生成exerciseFavorite.db的文件名称，方便出现问题及时回溯
 exerciseFavorite_name = "exerciseFavorite_" + "{:02d}{:02d}{:02d}{:02d}{:02d}{:02d}".format(current_time.tm_year, current_time.tm_mon, current_time.tm_mday, current_time.tm_hour, current_time.tm_min, current_time.tm_sec) + ".db"
 #生成并执行pull命令
-exerciseFavorite_pull_command = "adb pull /userdisk/math/exerciseFav/exerciseFavorite.db " + exerciseFavorite_name
-os.system(exerciseFavorite_pull_command) 
-print("已成功导出exerciseFavorite.db")
+if remote_copy_status == 1:
+    sftp.connect(ip_address, "root", dictpen_root)
+    local_file_path = './' + exerciseFavorite_name  # 当前目录
+    sftp.pull_file("/userdisk/math/exerciseFav/exerciseFavorite.db", local_file_path) 
+    print("已成功导出exerciseFavorite.db")
+else:
+    #检查设备是否处于OK状态
+    check_devices_okay()
+    #生成并执行pull命令
+    exerciseFavorite_pull_command = "adb pull /userdisk/math/exerciseFav/exerciseFavorite.db " + exerciseFavorite_name
+    os.system(exerciseFavorite_pull_command) 
+    print("已成功导出exerciseFavorite.db")
+
 
 #连接到数据库
 conn = sqlite3.connect(exerciseFavorite_name)
@@ -177,9 +323,8 @@ for file_dict in files_list:
     #由于实时获取timestamp会导致timestamp重复，反正不重要，就让其+1000ms就好了
     now_timestamp = now_timestamp + 1000
 
-    ans_content = "video_name:" + name + "\nvideo_md5:" + md5 + "timestamp:" + str(now_timestamp)
-    new_data = (md5,"2",md5,"1",
-                md5,None,None,"2",ans_content,
+    new_data = (md5,"2",name,2,
+                md5,None,None,"1","Video:" + name,
                 "1",None,None,None,'{"knowId":1164,"knowName":"求比值的方法","knowVideo":"'+ dictpen_video_path + name + '"}',
                 None,None,final_knowledgeID,None,
                 "[]","dictpen","0","1","1",str(now_timestamp)
@@ -191,26 +336,39 @@ for file_dict in files_list:
     #现在table_math的处理完了，接下来去处理table_knowledge
     (final_name_show,extension) = os.path.splitext(os.path.basename(file_dict["path"])) #不考虑运行效率的情况，去掉后缀获取文件名
 
-    knowledge_parents = '[{"knowId": 1164,"knowName": "求比值的方法","knowVideo": "' + dictpen_video_path +  md5 + ".mp4" + '"}]'
-    new_knowledge_data = (final_knowledgeID,final_name_show,dictpen_video_path +  md5 + ".mp4",knowledge_parents,"[]","[]","1",now_timestamp)
-
+    knowledge_parents = '[{"knowId": 1164,"knowName": "求比值的方法","knowVideo":  "' + dictpen_video_path + name + '"}]'
+    new_knowledge_data = (final_knowledgeID,final_name_show,dictpen_video_path + name,knowledge_parents,"[]","[]","1",now_timestamp)
     cursor.execute("INSERT INTO table_knowledge (code, text, video, parents, children, simQuesList, sync_state, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", new_knowledge_data)
-
-    #拷贝视频文件
-    if copy_video_to_dictpen_state == 1:
-        video_path = dictpen_video_path.replace("file://", "")
-        adb_command = "adb push " + '"' + file_dict["path"] + '" "' + video_path + md5 + ".mp4" + '"'
-        os.system(adb_command)
-
-#提交并关闭数据库
 conn.commit()
 conn.close()
+    #拷贝视频文件
+for file_dict in files_list:
+    if copy_video_to_dictpen_state == 1 and remote_copy_status == 0:
+        video_path = dictpen_video_path.replace("file://", "")
+        adb_command = "adb push " + '"' + file_dict["path"] + '" "' + video_path + name + '"'
+        os.system(adb_command)
+    else:
+        break
+if copy_video_to_dictpen_state == 1 and remote_copy_status == 1:
+    sftp.connect(ip_address, "root", dictpen_root)
+    video_path = dictpen_video_path.replace("file://", "")
+    result = sftp._execute_sftp(files_list, video_path)
+    print(result)
+
 
 if copy_video_to_dictpen_state == 0:
     print("已完成对数据库的添加。\n请将" + directory_path + "内的视频文件传至词典笔的" + dictpen_video_path + "目录下，\n完成后，按下Enter来将数据库传回词典笔内。\n")
     os.system("pause")
 #确保adb连接可用
-check_devices_okay()
-os.system("adb push " + exerciseFavorite_name +" /userdisk/math/exerciseFav/exerciseFavorite.db ")
-print("处理完成!")
+
+
+if remote_copy_status == 1:
+    sftp.connect(ip_address, "root", dictpen_root)
+    local_file_path = './' + exerciseFavorite_name  # 当前目录
+    sftp.execute_sftp(local_file_path, "/userdisk/math/exerciseFav/exerciseFavorite.db")
+    print("处理完成")
+else:
+    check_devices_okay()
+    os.system("adb push " + exerciseFavorite_name +" /userdisk/math/exerciseFav/exerciseFavorite.db ")
+    print("处理完成!")
 os.system("pause")
--- launcher.py
+++ launcher.py
@@ -4,6 +4,139 @@ import os
 import subprocess
 import sqlite3
 import time
+import paramiko
+
+class SFTP:
+    def __init__(self):
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.connection_start_time = None
+
+    def connect(self, ip, username, password):
+        self.connection_start_time = time.time()
+        try:
+            self.ssh_client.connect(ip, username=username, password=password, timeout=10)
+        except paramiko.AuthenticationException:
+            print("Authentication failed. Please check your credentials.")
+        except paramiko.SSHException as e:
+            end_time = time.time()
+            wait_time = int(end_time - self.connection_start_time)
+            print(f"Connection failed after {wait_time} seconds: {e}")
+        except Exception as e:
+            end_time = time.time()
+            wait_time = int(end_time - self.connection_start_time)
+            print(f"Error: {e}")
+
+    def bytes_to_mib(self, bytes_value):
+        mib_value = bytes_value / (1024 * 1024)
+        return mib_value
+
+    def _execute_sftp(self, local_files, remote_path):
+        # 连接SSH服务器
+        if not self.ssh_client.get_transport() or not self.ssh_client.get_transport().is_active():
+            print("SSH connection is not active.")
+            print("连接失败，请检查连接，按enter退出")
+            os.system("pause")
+            os._exit(1)
+
+        # 创建 SFTP 客户端
+        sftp_client = self.ssh_client.open_sftp()
+
+        # 上传文件到远程服务器
+        for file_info in local_files:
+            local_file_path = file_info["path"]
+            remote_file_path = os.path.join(remote_path, file_info["name"])
+            total_bytes = os.path.getsize(local_file_path)
+
+            # 检查远程文件是否存在，并获取已传输的大小
+            try:
+                remote_file_size = sftp_client.stat(remote_file_path).st_size
+            except FileNotFoundError:
+                remote_file_size = 0
+
+            # 定义进度回调函数
+            def progress_callback(bytes_transferred, file_size):
+                bytes_transferred_mib = self.bytes_to_mib(bytes_transferred)
+                file_size_mib = self.bytes_to_mib(file_size)
+                progress = int(bytes_transferred_mib / file_size_mib * 100)
+                print(f"\rProgress: {progress}% ({bytes_transferred_mib:.2f} MiB / {file_size_mib:.2f} MiB) - Uploading: {file_info['name']} - Elapsed Time: {self.get_elapsed_time()}", end='', flush=True)
+
+                # 判断是否上传完成
+                if bytes_transferred == file_size:
+                    print(f"\nUpload complete: {file_info['name']}")
+
+            # 上传文件
+            retries = 3  # 设置重试次数
+            for attempt in range(retries):
+                try:
+                    with open(local_file_path, 'rb') as local_file:
+                        # 移动文件指针至已传输的位置
+                        local_file.seek(remote_file_size)
+                        # 从已传输位置开始上传文件，并传入进度回调函数
+                        sftp_client.putfo(local_file, remote_file_path, callback=lambda x, y: progress_callback(x + remote_file_size, total_bytes), file_size=total_bytes)
+                    break  # 上传成功，跳出重试循环
+                except paramiko.SSHException as e:
+                    print(f"SSHException occurred during upload: {e}")
+                    if attempt < retries - 1:
+                        print("Retrying upload...")
+                        time.sleep(5)  # 等待一段时间后重试
+                    else:
+                        print("Upload failed after retries.")
+                        raise  # 上传失败，抛出异常
+
+        # 关闭连接
+        sftp_client.close()
+
+        print("\nAll files uploaded.")
+
+        # 返回上传完成信息
+        return "All files uploaded."
+
+    
+    def execute_sftp(self, local_files, remote_path):
+        if not self.ssh_client.get_transport() or not self.ssh_client.get_transport().is_active():
+            print("SSH connection is not active.")
+            print("连接失败，请检查连接，按enter退出")
+            os.system("pause")
+            os._exit(1)
+        sftp_client = self.ssh_client.open_sftp()
+        local_file_path = local_files
+        remote_file_path = remote_path
+        total_bytes = os.path.getsize(local_file_path)
+        with open(local_file_path, 'rb') as local_file:
+            sftp_client.putfo(local_file, remote_file_path)
+        sftp_client.close()
+        print("\nAll files uploaded.")
+        return "All files uploaded."
+
+
+    def get_elapsed_time(self):
+        if self.connection_start_time is not None:
+            elapsed_time = int(time.time() - self.connection_start_time)
+            return f"{elapsed_time} seconds"
+        else:
+            return "N/A"
+
+    def pull_file(self, remote_path, local_path):
+        if not self.ssh_client.get_transport() or not self.ssh_client.get_transport().is_active():
+            print("SSH connection is not active.")
+            print("连接失败，请检查连接，按enter退出")
+            os.system("pause") 
+            os._exit(1)
+        # 创建 SFTP 客户端
+        sftp_client = self.ssh_client.open_sftp()
+
+        # 下载文件
+        try:
+            sftp_client.get(remote_path, local_path)
+            print(f"File downloaded successfully to {local_path}")
+        except Exception as e:
+            print(f"Error downloading file: {e}")
+
+        # 关闭连接
+        sftp_client.close()
+
+sftp = SFTP()
 
 
 ####
@@ -81,16 +214,22 @@ def initialize():
     dictpen_password = config.get("dictpen_password","")
     copy_video_to_dictpen = config.get("copy_video_to_dictpen","")
     delete_video_from_dictpen = config.get("delete_video_from_dictpen","")
+    remote_copy_video_to_dictpen = config.get("remote_copy_video_to_dictpen","")
+    ip_address = config.get("ip_address","")
+    dictpen_root = config.get("dictpen_root","")
 
     layout = [
         [sg.Text("请修改您需要的参数，然后点击“确定并启动”按钮来启动脚本\n默认情况下，不建议随便更改陌生的数值。", font={'family': 'SimHei', 'size': 20, 'weight': 'bold'})],
         [sg.Text("dictpen_password【词典笔的adb密码，请先填入此项】："),sg.In(key='dictpen_password_input', default_text=dictpen_password),sg.Button("密码帮助")],
+        [sg.Text("dictpen_ip【词典笔的ip地址，线传非必填，当使用远程传输时必须填写此项】："),sg.In(key='dictpen_ip_input', default_text=ip_address)],
+        [sg.Text("dictpen_root【词典笔的root密码，线传非必填，当使用远程传输时必须填写此项】："),sg.In(key='dictpen_root_input',default_text=dictpen_root)],
         [sg.Text("table_name【表table_mathexercise的完整名称】："),sg.In(key='table_name_input', default_text=table_name), sg.Button("自动填充")],
         [sg.Text("video_input_path【输入视频的文件夹路径】："),sg.In(key='video_input_path_input', default_text=video_input_path), sg.FolderBrowse(button_text="选择文件夹", target='video_input_path_input')],
         [sg.Text("dictpen_video_path【词典笔存放视频的文件夹路径】："),sg.In(key='dictpen_video_path_input', default_text=dictpen_video_path),sg.Button("帮助")],
         [sg.Checkbox("在删除脚本中，将项目从词典笔删除时，同时删除词典笔上的视频文件（即执行rm）",default=copy_video_to_dictpen == 1, key="delete_video_from_dictpen_checkbox")],
         [sg.Checkbox("在添加脚本中，将项目添加到词典笔时，同时拷入电脑上的视频文件（即执行adb push）",default=delete_video_from_dictpen == 1, key="copy_video_to_dictpen_checkbox")],
-        [sg.Button("保存并启动删除脚本"), sg.Button("保存并启动添加脚本")]
+        [sg.Checkbox("远程进行操作（即利用ssh）",default=remote_copy_video_to_dictpen == 0, key="remote_copy_video_to_dictpen_checkbox")],
+        [sg.Button("保存并启动删除脚本"), sg.Button("保存并启动添加脚本"),sg.Button("设置开机自启ssh")]
     ]
 
 
@@ -112,29 +251,39 @@ def main():
             video_input_path_to_save = values["video_input_path_input"]
             vdictpen_video_path_to_save = values["dictpen_video_path_input"]
             vdictpen_password = values["dictpen_password_input"]
+            ip_address = values["dictpen_ip_input"]
+            dictpen_root = values["dictpen_root_input"]
             copy_video_to_dictpen = 1 if values["copy_video_to_dictpen_checkbox"] else 0
             delete_video_from_dictpen = 1 if values["delete_video_from_dictpen_checkbox"] else 0
-
+            remote_copy_video_to_dictpen = 1 if values["remote_copy_video_to_dictpen_checkbox"] else 0
             data = {
                 "table_name": table_name_to_save,
                 "video_input_path": video_input_path_to_save,
                 "dictpen_video_path": vdictpen_video_path_to_save,
                 "dictpen_password":vdictpen_password,
                 "copy_video_to_dictpen": copy_video_to_dictpen,
-                "delete_video_from_dictpen": delete_video_from_dictpen
+                "delete_video_from_dictpen": delete_video_from_dictpen,
+                "remote_copy_video_to_dictpen": remote_copy_video_to_dictpen,
+                "dictpen_root": dictpen_root,
+                "ip_address": ip_address
             }
 
             with open("config.json", "w", encoding="UTF-8") as file_object:
                 json.dump(data, file_object)
-
-            #检查设备是否处于OK状态
-            check_devices_okay()
+            remote_copy_status = remote_copy_video_to_dictpen
             current_time = time.localtime()
-            #生成exerciseFavorite.db的文件名称，方便出现问题及时回溯
             exerciseFavorite_name = "exerciseFavorite_" + "{:02d}{:02d}{:02d}{:02d}{:02d}{:02d}".format(current_time.tm_year, current_time.tm_mon, current_time.tm_mday, current_time.tm_hour, current_time.tm_min, current_time.tm_sec) + ".db"
-            #生成并执行pull命令
-            exerciseFavorite_pull_command = "adb pull /userdisk/math/exerciseFav/exerciseFavorite.db " + exerciseFavorite_name
-            os.system(exerciseFavorite_pull_command) 
+            if remote_copy_status == 1:
+                sftp.connect(ip_address, "root", dictpen_root)
+                local_file_path = './' + exerciseFavorite_name  # 当前目录
+                sftp.pull_file("/userdisk/math/exerciseFav/exerciseFavorite.db", local_file_path) 
+            #检查设备是否处于OK状态
+            else:
+                check_devices_okay()
+                #生成exerciseFavorite.db的文件名称，方便出现问题及时回溯
+                #生成并执行pull命令
+                exerciseFavorite_pull_command = "adb pull /userdisk/math/exerciseFav/exerciseFavorite.db " + exerciseFavorite_name
+                os.system(exerciseFavorite_pull_command) 
 
             conn = sqlite3.connect(exerciseFavorite_name)
             cursor = conn.cursor()
@@ -159,22 +308,53 @@ def main():
             video_input_path_to_save = values["video_input_path_input"]
             vdictpen_video_path_to_save = values["dictpen_video_path_input"]
             vdictpen_password = values["dictpen_password_input"]
+            ip_address = values["dictpen_ip_input"]
+            dictpen_root = values["dictpen_root_input"]
             copy_video_to_dictpen = 1 if values["copy_video_to_dictpen_checkbox"] else 0
             delete_video_from_dictpen = 1 if values["delete_video_from_dictpen_checkbox"] else 0
-
+            remote_copy_video_to_dictpen = 1 if values["remote_copy_video_to_dictpen_checkbox"] else 0
             data = {
                 "table_name": table_name_to_save,
                 "video_input_path": video_input_path_to_save,
                 "dictpen_video_path": vdictpen_video_path_to_save,
                 "dictpen_password":vdictpen_password,
                 "copy_video_to_dictpen": copy_video_to_dictpen,
-                "delete_video_from_dictpen": delete_video_from_dictpen
+                "delete_video_from_dictpen": delete_video_from_dictpen,
+                "remote_copy_video_to_dictpen": remote_copy_video_to_dictpen,
+                "dictpen_root": dictpen_root,
+                "ip_address": ip_address
             }
 
             with open("config.json", "w", encoding="UTF-8") as file_object:
                 json.dump(data, file_object)
 
-            os.system('start item_add.py')
+            os.system('item_add.py')
+
+        elif event == "设置开机自启ssh":
+            table_name_to_save = values["table_name_input"]
+            video_input_path_to_save = values["video_input_path_input"]
+            vdictpen_video_path_to_save = values["dictpen_video_path_input"]
+            vdictpen_password = values["dictpen_password_input"]
+            ip_address = values["dictpen_ip_input"]
+            dictpen_root = values["dictpen_root_input"]
+            copy_video_to_dictpen = 1 if values["copy_video_to_dictpen_checkbox"] else 0
+            delete_video_from_dictpen = 1 if values["delete_video_from_dictpen_checkbox"] else 0
+            remote_copy_video_to_dictpen = 1 if values["remote_copy_video_to_dictpen_checkbox"] else 0
+            data = {
+                "table_name": table_name_to_save,
+                "video_input_path": video_input_path_to_save,
+                "dictpen_video_path": vdictpen_video_path_to_save,
+                "dictpen_password":vdictpen_password,
+                "copy_video_to_dictpen": copy_video_to_dictpen,
+                "delete_video_from_dictpen": delete_video_from_dictpen,
+                "remote_copy_video_to_dictpen": remote_copy_video_to_dictpen,
+                "dictpen_root": dictpen_root,
+                "ip_address": ip_address
+            }
+            with open("config.json", "w", encoding="UTF-8") as file_object:
+                json.dump(data, file_object)
+                            
+            os.system('start ssh.py')
 
         elif event == "保存并启动删除脚本":
             #先保存adb密码
@@ -182,16 +362,21 @@ def main():
             video_input_path_to_save = values["video_input_path_input"]
             vdictpen_video_path_to_save = values["dictpen_video_path_input"]
             vdictpen_password = values["dictpen_password_input"]
+            ip_address = values["dictpen_ip_input"]
+            dictpen_root = values["dictpen_root_input"]
             copy_video_to_dictpen = 1 if values["copy_video_to_dictpen_checkbox"] else 0
             delete_video_from_dictpen = 1 if values["delete_video_from_dictpen_checkbox"] else 0
-
+            remote_copy_video_to_dictpen = 1 if values["remote_copy_video_to_dictpen_checkbox"] else 0
             data = {
                 "table_name": table_name_to_save,
                 "video_input_path": video_input_path_to_save,
                 "dictpen_video_path": vdictpen_video_path_to_save,
                 "dictpen_password":vdictpen_password,
                 "copy_video_to_dictpen": copy_video_to_dictpen,
-                "delete_video_from_dictpen": delete_video_from_dictpen
+                "delete_video_from_dictpen": delete_video_from_dictpen,
+                "remote_copy_video_to_dictpen": remote_copy_video_to_dictpen,
+                "dictpen_root": dictpen_root,
+                "ip_address": ip_address
             }
 
             with open("config.json", "w", encoding="UTF-8") as file_object:
